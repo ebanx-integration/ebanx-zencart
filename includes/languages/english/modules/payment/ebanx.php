c6c607c8c194fe53e3787c92b4f7bcecf748a024
heitor <heitor@ebanx.com>
2014-10-06 10:55:05 -0300

    Corrected library bug

---
+++ includes/languages/english/modules/payment/ebanx.php
@@ -0,0 +1,13 @@
+<?php
+
+  define('MODULE_PAYMENT_EBANX_TEXT_TITLE', 'EBANX');
+  define('MODULE_PAYMENT_EBANX_TEXT_DESCRIPTION', 'Collect payments using EBANX network.');
+  define('MODULE_PAYMENT_EBANX_TEXT_PUBLIC_TITLE', 'EBANX');
+  define('MODULE_PAYMENT_EBANX_TEXT_CREDIT_CARD_OWNER', 'Credit Card Owner:');
+  define('MODULE_PAYMENT_EBANX_TEXT_CREDIT_CARD_NUMBER', 'Credit Card Number:');
+  define('MODULE_PAYMENT_EBANX_TEXT_CREDIT_CARD_EXPIRES', 'Credit Card Expiry Date:');
+  define('MODULE_PAYMENT_EBANX_TEXT_CVV', 'CVV Number:');
+  define('MODULE_PAYMENT_EBANX_TEXT_POPUP_CVV_LINK', 'What\'s this?');
+  define('MODULE_PAYMENT_EBANX_TEXT_CUSTOMER_CPF', 'CPF');
+  
+  
---
+++ includes/modules/payment/ebanx/ebanx-php-master/examples/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+\Ebanx\Config::set(array(
+  'integrationKey' => '1231000',
+  'directMode'     => false,
+  'testMode'       => true
+));
