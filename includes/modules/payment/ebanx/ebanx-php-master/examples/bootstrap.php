ab4e795548bdbb4b4a92451c60511ccdd747ee08
Heitor D <heitor@ebanx.com>
2014-11-03 15:44:11 -0200

    Added submodule

--- includes/modules/payment/ebanx/ebanx-php-master/examples/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-\Ebanx\Config::set(array(
-  'integrationKey' => '1231000',
-  'directMode'     => false,
-  'testMode'       => true
-));
--- includes/modules/payment/ebanx/ebanx-php-master/examples/bootstrap.php~
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-\Ebanx\Config::set(array(
-  'integrationKey' => '',
-  'directMode'     => false,
-  'testMode'       => true
-));
